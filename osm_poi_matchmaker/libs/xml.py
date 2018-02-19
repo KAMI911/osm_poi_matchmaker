1c4bfc6e4f39cdedaa470ce1d5c9baddc62c747c
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-02-19 18:37:02 +0100

    Add cached mode and city name cleanups, postcode from XML file\n\n* Add cached mode and config parameter to use saved resources\n* More city name cleanups\n* Download postcode, city database as XML from Magyar Posta Partner Extra\nhttps://www.posta.hu/partnerextra\nResource: http://httpmegosztas.posta.hu/PartnerExtra/OUT/ZipCodes.xml

---
+++ osm_poi_matchmaker/libs/xml.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import traceback
+    import logging
+    import requests
+    import os
+    from lxml import etree
+    from osm_poi_matchmaker.utils import config
+except ImportError as err:
+    print('Error {0} import module: {1}'.format(__name__, err))
+    traceback.print_exc()
+    exit(128)
+
+
+def download_xml(link, verify_link=config.get_download_verify_link()):
+    try:
+        page = requests.get(link, verify=verify_link)
+    except requests.exceptions.ConnectionError as e:
+        logging.warning('Unable to open connection.')
+        return None
+    return page.content if page.status_code == 200 else None
+
+
+def save_downloaded_xml(link, file, verify=config.get_download_verify_link()):
+    if config.get_download_use_cached_data() == True:
+        with open(file, 'r') as content_file:
+             page = content_file.read()
+    else:
+        page = download_xml(link, verify)
+    if page != None:
+        if not os.path.exists(config.get_directory_cache_url()):
+            os.makedirs(config.get_directory_cache_url())
+        with open(file, mode='wb') as code:
+            code.write(page)
+    else:
+        logging.warning('Skipping dataset.')
+    return page
