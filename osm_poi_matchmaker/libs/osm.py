b8842bcb61f4388a0f2af8cd2d5cb472237b60ed
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-05-25 17:23:25 +0200

    Add postcode query based on POI position

--- osm_poi_matchmaker/libs/osm.py
+++ osm_poi_matchmaker/libs/osm.py
@@ -26,9 +26,18 @@ def query_osm_postcode_gpd(session, lon, lat):
     if lat is None or lat == '' or lon == '' or lon is None: return None
     query = sqlalchemy.text('''
         SELECT name
-        FROM planet_osm_polygon, (SELECT ST_SetSRID(ST_MakePoint(:lon,:lat),4326) as geom) point
+        FROM planet_osm_polygon, (SELECT ST_SetSRID(ST_MakePoint(:lat,:lon),4326) as geom) point
         WHERE boundary='postal_code' and ST_Contains(way, ST_Transform(point.geom,900913)) ORDER BY name LIMIT 1;''')
     data = session.execute(query, {'lon':lon, 'lat':lat}).first()
     if data is None: return None
     row = dict(zip(data.keys(), data))
     return int(row['name'].split(' ')[0]) if row['name'].split(' ')[0] is not None else None
+
+def query_postcode_osm_external(prefer_osm, session, lon, lat, postcode_ext):
+    if prefer_osm is False and postcode_ext is not None:
+        return postcode_ext
+    query_postcode = query_osm_postcode_gpd(session, lon, lat)
+    if prefer_osm is True and query_postcode is not None:
+        return query_postcode
+    elif prefer_osm is True and query_postcode is None:
+        return postcode_ext
