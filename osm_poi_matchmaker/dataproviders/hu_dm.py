5ccc11b3798069bbcab643798f1bdb60801012b7
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-05-27 11:55:11 +0200

    Add dm dataprovider

---
+++ osm_poi_matchmaker/dataproviders/hu_dm.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import traceback
+    import logging
+    import os
+    import json
+    import pandas as pd
+    from osm_poi_matchmaker.dao.data_handlers import insert_poi_dataframe
+    from osm_poi_matchmaker.libs.soup import save_downloaded_soup
+    from osm_poi_matchmaker.libs.address import extract_street_housenumber_better, clean_city, clean_opening_hours, clean_phone
+    from osm_poi_matchmaker.libs.geo import check_geom, check_hu_boundary
+    from osm_poi_matchmaker.libs.osm import query_postcode_osm_external
+    from osm_poi_matchmaker.dao import poi_array_structure
+except ImportError as err:
+    print('Error {0} import module: {1}'.format(__name__, err))
+    traceback.print_exc()
+    exit(128)
+
+
+POI_COLS = poi_array_structure.POI_COLS
+POI_DATA = 'https://www.dm.hu/cms/restws/stores/find?requestingCountry=HU&countryCodes=DE%2CAT%2CBA%2CBG%2CSK%2CRS%2CHR%2CCZ%2CRO%2CSI%2CHU%2CMK%2CIT&mandantId=870&bounds=46.599301%2C17.325265%7C47.71978%2C21.681344&before=false&after=false&morningHour=9&eveningHour=18&_=1527413070144'
+
+
+class hu_dm():
+
+    def __init__(self, session, download_cache, prefer_osm_postcode, filename='hu_dm.json'):
+        self.session = session
+        self.link = POI_DATA
+        self.download_cache = download_cache
+        self.prefer_osm_postcode = prefer_osm_postcode
+        self.filename = filename
+
+    @staticmethod
+    def types():
+        data = [{'poi_code': 'hudmche', 'poi_name': 'dm', 'poi_type': 'chemist',
+                 'poi_tags': "{'shop': 'chemist', 'operator': 'dm Kft.', 'brand':'dm', 'facebook':'https://www.facebook.com/dm.Magyarorszag', 'youtube': 'https://www.youtube.com/user/dmMagyarorszag', 'instagram':'https://www.instagram.com/dm_magyarorszag/', 'payment:cash': 'yes', 'payment:debit_cards': 'yes'}",
+                 'poi_url_base': 'https://www.dm.hu'}]
+        return data
+
+    def process(self):
+        soup = save_downloaded_soup('{}'.format(self.link), os.path.join(self.download_cache, self.filename))
+        insert_data = []
+        if soup != None:
+            text = json.loads(soup.get_text())
+            for poi_data in text:
+                name = 'dm'
+                code = 'hudmche'
+                postcode = poi_data['address']['plz'].strip()
+                street, housenumber, conscriptionnumber = extract_street_housenumber_better(
+                    poi_data['address']['street'])
+                city = clean_city(poi_data['address']['city'])
+                branch = None #poi_data['title']
+                website = None
+                nonstop = None
+                mo_o = None
+                th_o = None
+                we_o = None
+                tu_o = None
+                fr_o = None
+                sa_o = None
+                su_o = None
+                mo_c = None
+                th_c = None
+                we_c = None
+                tu_c = None
+                fr_c = None
+                sa_c = None
+                su_c = None
+                original = poi_data['address']['street']
+                ref = None
+                lat, lon = check_hu_boundary(poi_data['location'][0], poi_data['location'][1])
+                geom = check_geom(lat, lon)
+                postcode = query_postcode_osm_external(self.prefer_osm_postcode, self.session, lat, lon, postcode)
+                if 'telnr' in poi_data and poi_data['phone'] != '':
+                    phone = clean_phone(poi_data['phone'])
+                else:
+                    phone = None
+                email = None
+                insert_data.append(
+                    [code, postcode, city, name, branch, website, original, street, housenumber, conscriptionnumber,
+                     ref, phone, email, geom, nonstop, mo_o, th_o, we_o, tu_o, fr_o, sa_o, su_o, mo_c, th_c, we_c, tu_c, fr_c, sa_c, su_c])
+            if len(insert_data) < 1:
+                logging.warning('Resultset is empty. Skipping ...')
+            else:
+                df = pd.DataFrame(insert_data)
+                df.columns = POI_COLS
+                insert_poi_dataframe(self.session, df)
