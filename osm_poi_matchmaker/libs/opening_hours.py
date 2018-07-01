0d8cc1f3a869fa30d67602cfafce0301bd17d4b1
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-07-01 14:43:43 +0200

    Implement week days interval

--- osm_poi_matchmaker/libs/opening_hours.py
+++ osm_poi_matchmaker/libs/opening_hours.py
@@ -26,9 +26,11 @@ class OpeningHours(object):
         self.df_oh = pd.DataFrame.from_dict(self.opening_hours, orient = 'index', columns=self.oh_types)
         self.df_dup = self.df_oh.sort_values('did').drop_duplicates(['open', 'close'], keep='first')
         self.df_dup['same'] = None
-        nodes= [1,2,3,4]
+        self.df_dup['same_id'] = None
         for k, v in self.df_dup.iterrows():
             self.df_dup.at[k, 'same'] = self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])].index.tolist()
+            if self.df_dup['same'] is not None:
+                self.df_dup.at[k, 'same_id'] =  self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])]['did'].tolist()
 
 
     @property
@@ -54,7 +56,21 @@ class OpeningHours(object):
             return '24/7'
         for k, v in self.df_dup.iterrows():
             if v['open'] is not None and v['close'] is not None:
-                days = ','.join(sorted(v['same']))
+                # Order by week days
+                same_sorted = sorted(v['same'])
+                same_id_sorted = sorted(v['same_id'])
+                # Try to mage days interval
+                if len(same_id_sorted) >= 2:
+                    diffs = [same_id_sorted[i + 1] - same_id_sorted[i] for i in range(len(same_id_sorted) - 1)]
+                    # Diffs list contains only 1 to make day interval
+                    if diffs.count(1) == len(diffs):
+                        days = '{}-{}'.format(self.week_days[same_id_sorted[0]], self.week_days[same_id_sorted[-1]])
+                    # Make list of days
+                    else:
+                        days = ','.join(same_sorted)
+                # Make list of days
+                else:
+                    days = ','.join(same_sorted)
                 oh_list.append('{} {}-{}'.format(days.title(), self.df_dup.at[k, 'open'], self.df_dup.at[k, 'close']))
                 oh = ';'.join(oh_list)
         if oh_list == []:
