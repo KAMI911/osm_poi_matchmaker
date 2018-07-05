da7ddd6830fa0a0cb373e4c0288fe93a3697a2a3
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-07-05 09:19:31 +0200

    Fix day order in opening_hours list

--- osm_poi_matchmaker/libs/opening_hours.py
+++ osm_poi_matchmaker/libs/opening_hours.py
@@ -2,6 +2,7 @@
 
 try:
     import logging
+    import collections
     import pandas as pd
 except ImportError as err:
     print('Error {0} import module: {1}'.format(__name__, err))
@@ -27,11 +28,11 @@ class OpeningHours(object):
         self.df_oh = pd.DataFrame.from_dict(self.opening_hours, orient = 'index', columns=self.oh_types)
         self.df_dup = self.df_oh.sort_values('did').drop_duplicates(['open', 'close'], keep='first')
         self.df_dup['same'] = None
-        self.df_dup['same_id'] = None
         for k, v in self.df_dup.iterrows():
-            self.df_dup.at[k, 'same'] = self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])].index.tolist()
-            if self.df_dup['same'] is not None:
-                self.df_dup.at[k, 'same_id'] =  self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])]['did'].tolist()
+            same = self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])].index.tolist()
+            if same is not None:
+                same_id =  self.df_oh.loc[(self.df_oh['open'] == v['open']) & (self.df_oh['close'] == v['close'])]['did'].tolist()
+                self.df_dup.at[k, 'same'] = collections.OrderedDict(zip(same_id, same))
 
 
     @property
@@ -58,20 +59,21 @@ class OpeningHours(object):
         for k, v in self.df_dup.iterrows():
             if v['open'] is not None and v['close'] is not None:
                 # Order by week days
-                same_sorted = sorted(v['same'])
-                same_id_sorted = sorted(v['same_id'])
-                # Try to mage days interval
-                if len(same_id_sorted) >= 2:
-                    diffs = [same_id_sorted[i + 1] - same_id_sorted[i] for i in range(len(same_id_sorted) - 1)]
+                ordered = collections.OrderedDict(sorted(v['same'].items(), key=lambda x: x[0]))
+                same = list(ordered.values())
+                # Try to merge days interval
+                if len(ordered) >= 2:
+                    same_id = list(ordered.keys())
+                    diffs = [same_id[i + 1] - same_id[i] for i in range(len(same_id) - 1)]
                     # Diffs list contains only 1 to make day interval
                     if diffs.count(1) == len(diffs):
-                        days = '{}-{}'.format(self.week_days[same_id_sorted[0]], self.week_days[same_id_sorted[-1]])
+                        days = '{}-{}'.format(list(ordered.values())[0], list(ordered.values())[-1])
                     # Make list of days
                     else:
-                        days = ','.join(same_sorted)
+                        days = ','.join(same)
                 # Make list of days
                 else:
-                    days = ','.join(same_sorted)
+                    days = ','.join(same)
                 if self.lunchbreak['start'] is None and self.lunchbreak['stop'] is None:
                     oh_list.append(
                         '{} {}-{}'.format(days.title(), self.df_dup.at[k, 'open'], self.df_dup.at[k, 'close']))
