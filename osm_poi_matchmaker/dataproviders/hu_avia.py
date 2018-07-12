b645069cdf29b8ae4e814ed1e75c5c21e71cd304
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-07-12 17:11:45 +0200

    Refactor to use POIDataset class, code cleanups

--- osm_poi_matchmaker/dataproviders/hu_avia.py
+++ osm_poi_matchmaker/dataproviders/hu_avia.py
@@ -13,14 +13,12 @@ try:
         clean_email
     from osm_poi_matchmaker.libs.geo import check_geom, check_hu_boundary
     from osm_poi_matchmaker.libs.osm import query_postcode_osm_external
-    from osm_poi_matchmaker.libs.opening_hours import OpeningHours
-    from osm_poi_matchmaker.dao import poi_array_structure
+    from osm_poi_matchmaker.libs.poi_dataset import POIDataset
 except ImportError as err:
     print('Error {0} import module: {1}'.format(__name__, err))
     traceback.print_exc()
     exit(128)
 
-POI_COLS = poi_array_structure.POI_COLS
 POI_DATA = 'https://www.avia.hu/kapcsolat/toltoallomasok/'
 
 
@@ -52,74 +50,30 @@ class hu_avia():
             data = data.replace("'", '"')
             data = clean_javascript_variable(data, 'markers')
             text = json.loads(data)
+            data = POIDataset()
             for poi_data in text:
                 if poi_data['cim'] is not None and poi_data['cim'] != '':
-                    postcode, city, street, housenumber, conscriptionnumber = extract_all_address(poi_data['cim'])
-                name = 'Avia'
-                code = 'huaviafu'
-                branch = None
-                if city is None:
-                    city = poi_data['title']
-                ref = poi_data['kutid'] if poi_data['kutid'] is not None and poi_data['kutid'] != '' else None
-                lat, lon = check_hu_boundary(poi_data['lat'], poi_data['lng'])
-                geom = check_geom(lat, lon)
-                postcode = query_postcode_osm_external(self.prefer_osm_postcode, self.session, lat, lon, postcode)
-                website = '/toltoallomas/?id={}'.format(str(poi_data['kutid'])) if poi_data['kutid'] is not None and \
+                    data.postcode, data.city, data.street, data.housenumber, data.conscriptionnumber = extract_all_address(poi_data['cim'])
+                data.name = 'Avia'
+                data.code = 'huaviafu'
+                if data.city is None:
+                    data.city = poi_data['title']
+                data.ref = poi_data['kutid'] if poi_data['kutid'] is not None and poi_data['kutid'] != '' else None
+                data.lat, data.lon = check_hu_boundary(poi_data['lat'], poi_data['lng'])
+                data.postcode = query_postcode_osm_external(self.prefer_osm_postcode, self.session, data.lat, data.lon, data.postcode)
+                data.website = '/toltoallomas/?id={}'.format(str(poi_data['kutid'])) if poi_data['kutid'] is not None and \
                                                                                    poi_data['kutid'] != '' else None
-                nonstop = None
-                mo_o = None
-                tu_o = None
-                we_o = None
-                th_o = None
-                fr_o = None
-                sa_o = None
-                su_o = None
-                mo_c = None
-                tu_c = None
-                we_c = None
-                th_c = None
-                fr_c = None
-                sa_c = None
-                su_c = None
-                summer_mo_o = None
-                summer_tu_o = None
-                summer_we_o = None
-                summer_th_o = None
-                summer_fr_o = None
-                summer_sa_o = None
-                summer_su_o = None
-                summer_mo_c = None
-                summer_tu_c = None
-                summer_we_c = None
-                summer_th_c = None
-                summer_fr_c = None
-                summer_sa_c = None
-                summer_su_c = None
-                lunch_break_start = None
-                lunch_break_stop = None
-                t = OpeningHours(nonstop, mo_o, tu_o, we_o, th_o, fr_o, sa_o, su_o, mo_c, tu_c, we_c, th_c, fr_c, sa_c,
-                                 su_c, summer_mo_o, summer_tu_o, summer_we_o, summer_th_o, summer_fr_o, summer_sa_o,
-                                 summer_su_o, summer_mo_c, summer_tu_c, summer_we_c, summer_th_c, summer_fr_c,
-                                 summer_sa_c, summer_su_c, lunch_break_start, lunch_break_stop)
-                opening_hours = t.process()
-                original = poi_data['cim']
+                data.original = poi_data['cim']
                 if 'tel' in poi_data and poi_data['tel'] != '':
-                    phone = clean_phone(poi_data['tel'])
+                    data.phone = clean_phone(poi_data['tel'])
                 else:
-                    phone = None
+                    data.phone = None
                 if 'email' in poi_data and poi_data['email'] != '':
-                    email = clean_email(poi_data['email'])
+                    data.email = clean_email(poi_data['email'])
                 else:
-                    email = None
-                insert_data.append(
-                    [code, postcode, city, name, branch, website, original, street, housenumber, conscriptionnumber,
-                     ref, phone, email, geom, nonstop, mo_o, tu_o, we_o, th_o, fr_o, sa_o, su_o, mo_c, tu_c, we_c, th_c,
-                     fr_c, sa_c, su_c, summer_mo_o, summer_tu_o, summer_we_o, summer_th_o, summer_fr_o, summer_sa_o,
-                     summer_su_o, summer_mo_c, summer_tu_c, summer_we_c, summer_th_c,
-                     summer_fr_c, summer_sa_c, summer_su_c, lunch_break_start, lunch_break_stop, opening_hours])
-            if len(insert_data) < 1:
+                    data.email = None
+                data.add()
+            if data.lenght() < 1:
                 logging.warning('Resultset is empty. Skipping ...')
             else:
-                df = pd.DataFrame(insert_data)
-                df.columns = POI_COLS
-                insert_poi_dataframe(self.session, df)
+                insert_poi_dataframe(self.session, data.process())
