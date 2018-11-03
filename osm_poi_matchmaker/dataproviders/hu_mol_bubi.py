e5db4d9bf79d3f97104ca1b31ead088ca4a6d8d9
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-11-03 08:13:22 +0100

    Add twitter and instagram, remove payment method

--- osm_poi_matchmaker/dataproviders/hu_mol_bubi.py
+++ osm_poi_matchmaker/dataproviders/hu_mol_bubi.py
@@ -29,7 +29,7 @@ class hu_mol_bubi(DataProvider):
 
     def types(self):
         self.__types = [{'poi_code': 'hububibir', 'poi_name': 'MOL Bubi', 'poi_type': 'bicycle_rental',
-                 'poi_tags': "{'amenity': 'bicycle_rental', 'brand': 'MOL Bubi', 'operator': 'BKK MOL Bubi', 'network': 'bubi', 'addr:country': 'HU', 'payment:credit_cards': 'yes', 'facebook': 'https://www.facebook.com/molbubi', 'youtube': 'https://www.youtube.com/user/bkkweb'}",
+                 'poi_tags': "{'amenity': 'bicycle_rental', 'brand': 'MOL Bubi', 'operator': 'BKK MOL Bubi', 'network': 'bubi', 'addr:country': 'HU', 'instagram': 'https://www.instagram.com/molbubi/', 'facebook': 'https://www.facebook.com/molbubi', 'youtube': 'https://www.youtube.com/user/bkkweb', 'twitter': 'molbubi'}",
                  'poi_url_base': 'https://molbubi.bkk.hu', 'poi_search_name': '(mol bubi|bubi)'}]
         return self.__types
 
