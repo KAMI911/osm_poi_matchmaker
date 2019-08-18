33b00d6e7ae435785c1867e67de8fabfb28ed626
Kálmán „KAMI” Szalai <kami911@gmail.com>
2019-08-18 06:31:46 +0200

    Hande files as UTF-8 files

--- osm_poi_matchmaker/libs/xml.py
+++ osm_poi_matchmaker/libs/xml.py
@@ -24,15 +24,15 @@ def download_xml(link, verify_link=config.get_download_verify_link()):
 
 def save_downloaded_xml(link, file, verify=config.get_download_verify_link()):
     if config.get_download_use_cached_data() == True and os.path.isfile(file):
-        with open(file, 'rb') as content_file:
+        with open(file, 'r', encoding='utf-8') as content_file:
             page = content_file.read()
     else:
         page = download_xml(link, verify)
         if page != None:
             if not os.path.exists(config.get_directory_cache_url()):
                 os.makedirs(config.get_directory_cache_url())
-            with open(file, mode='wb') as code:
-                code.write(page)
+            with open(file, mode='w', encoding='utf-8') as code:
+                code.write(page.decode('utf-8'))
         else:
             logging.warning('Skipping dataset.')
     return page
