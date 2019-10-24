33640d1331b68c8a3ee94296a96bad4963186e93
Kálmán „KAMI” Szalai <kami911@gmail.com>
2019-10-24 08:55:50 +0200

    Update HU/OMV POI common data
    Fixes #32

--- osm_poi_matchmaker/dataproviders/hu_omv.py
+++ osm_poi_matchmaker/dataproviders/hu_omv.py
@@ -32,7 +32,7 @@ class hu_omv(DataProvider):
 
     def types(self):
         self.__types = [{'poi_code': 'huomvfu', 'poi_name': 'OMV', 'poi_type': 'fuel',
-                 'poi_tags': "{'amenity': 'fuel', 'brand': 'OMV', 'operator': 'OMV Hungária Kft.', 'addr:country': 'HU', 'payment:cash': 'yes', 'payment:debit_cards': 'yes', 'fuel:diesel': 'yes', 'fuel:octane_95': 'yes', 'air_conditioning': 'yes'}",
+                 'poi_tags': "{'amenity': 'fuel', 'name': 'OMV', 'brand': 'OMV', 'payment:cash': 'yes', 'payment:debit_cards': 'yes', 'fuel:diesel': 'yes', 'fuel:octane_95': 'yes', 'air_conditioning': 'yes', 'brand:wikidata': 'Q168238', 'brand:wikipedia': 'en:OMV', 'operator': 'OMV Hungária Kft.', 'operator:addr': '1117 Budapest, Október huszonharmadika utca 6-10 5. emelet 5/A.', 'ref:vatin:hu': '10542925-2-44', 'ref:vatin': 'HU10542925', 'ref:HU:company': '01-09-071584', 'contact:email': 'info.hungary@omv.com', 'contact:facebook': 'https://www.facebook.com/omvmagyarorszag', 'contact:fax': '+36 1 381 9899', 'contact:twitter': 'omv', 'contact:linkedin:https://www.linkedin.com/company/omv', 'contact:instagram': 'https://www.instagram.com/omv/', 'contact:youtube': 'https://www.youtube.com/user/omvofficial', 'contact:phone': '+36 1 381 9700', 'contact:website': 'http://www.omv.hu/'}",
                  'poi_url_base': 'https://www.omv.hu', 'poi_search_name': 'omv', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200, 'osm_search_distance_unsafe': 5}]
         return self.__types
 
