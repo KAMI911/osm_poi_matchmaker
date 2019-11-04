20d2be1a765709cf2c15a77f5e97d51cabeefb9f
Kálmán „KAMI” Szalai <kami911@gmail.com>
2019-11-04 11:33:16 +0100

    Add other payment methods templates and use them in data providers
    Issue #37

--- osm_poi_matchmaker/libs/osm_tag_sets.py
+++ osm_poi_matchmaker/libs/osm_tag_sets.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-POS_OTP = " 'payment:cash': 'yes', 'payment:contactless': 'yes', 'payment:mastercard': 'yes'," \
+POS_OTP = " 'payment:contactless': 'yes', 'payment:mastercard': 'yes'," \
           " 'payment:visa': 'yes', 'payment:erzsebet': 'yes', 'payment:erzsebet_plus': 'yes'," \
           " 'payment:edenred_voucher': 'yes', 'payment:edenred_card': 'yes', 'payment:otp_cafeteria': 'yes', " \
           " 'payment:multipay': 'yes', 'payment:mastercard_electronic': 'yes', 'payment:maestro': 'yes', " \
           " 'payment:visa_electron': 'yes', 'payment:mastercard_contactless': 'yes', 'payment:otp_bankpont': 'yes', " \
           " 'payment:american_express': 'yes', 'payment:jcb': 'yes', 'payment:v_pay': 'yes', " \
           " 'payment:unionpay': 'yes', "
+
+POS_HU_GEN  = " 'payment:contactless': 'yes', 'payment:mastercard': 'yes'," \
+              " 'payment:visa': 'yes', 'payment:mastercard_electronic': 'yes', 'payment:maestro': 'yes', " \
+              " 'payment:visa_electron': 'yes', 'payment:mastercard_contactless': 'yes', "
+
+PAY_CASH = " 'payment:cash': 'yes', "
