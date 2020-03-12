90b9ca62cb04b23c907f56f8a0b3110fe0cbe190
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-03-12 07:21:44 +0100

    The Hungarian Kulcs Patika data provider is broken - this is the fix for it
    Resolving all of these problems:
    
    * The address has changed
    * Download headers, data structure has changed
    
    Fixes #59
    
    Uniform data providers
    
    Issue #57

--- osm_poi_matchmaker/dataproviders/hu_kulcs_patika.py
+++ osm_poi_matchmaker/dataproviders/hu_kulcs_patika.py
@@ -20,45 +20,61 @@ except ImportError as err:
 
 class hu_kulcs_patika(DataProvider):
 
-
     def constains(self):
         self.link = 'https://old.kulcspatikak.hu/_facebook/inc/getPagerContent.php?tipus=patika&kepnelkul=true&latitude=19.040164947509766&longitude=47.49801180144693'
         self.POI_COMMON_TAGS = ""
         self.filename = self.filename + 'json'
-        self.headers = {'Referer': 'https://kulcspatikak.hu/patikakereso', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0', 'Accept': 'application/json, text/javascript, */*; q=0.01'}
+        self.headers = {'Referer': 'https://kulcspatikak.hu/patikakereso',
+                        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0',
+                        'Accept': 'application/json, text/javascript, */*; q=0.01'}
         self.post = {'kepnelkul': 'true', 'latitude': '47.498', 'longitude': '19.0399', 'tipus': 'patika'}
 
     def types(self):
         self.__types = [{'poi_code': 'hukulcspha', 'poi_name': 'Kulcs Patika', 'poi_type': 'pharmacy',
-                 'poi_tags': "{'amenity': 'pharmacy', 'brand': 'Kulcs Patika', 'dispensing': 'yes', " + POS_HU_GEN + PAY_CASH + "'air_conditioning': 'yes'}",
-                 'poi_url_base': 'https://www.kulcspatika.hu/', 'poi_search_name': '(kulcs patika|kulcs)', 'preserve_original_name': True}]
+                 'poi_tags': "{'amenity': 'pharmacy', 'brand': 'Kulcs Patika', 'dispensing': 'yes', "
+                             + POS_HU_GEN + PAY_CASH + "'air_conditioning': 'yes'}",
+                 'poi_url_base': 'https://www.kulcspatika.hu/', 'poi_search_name': '(kulcs patika|kulcs)',
+                         'preserve_original_name': True}]
         return self.__types
 
     def process(self):
         try:
             if self.link:
-                soup = save_downloaded_soup('{}'.format(self.link), os.path.join(self.download_cache,
-                                            self.filename), self.post_data, self.verify_link, headers=self.headers)
-                if soup is not None:
-                    text = json.loads(soup.get_text())
-                    for poi_data in text:
-                        if 'Kulcs patika' not in poi_data['nev']:
-                            self.data.name = poi_data['nev'].strip()
-                            self.data.branch = None
-                        else:
-                            self.data.name = 'Kulcs patika'
-                            self.data.branch = poi_data['nev'].strip()
-                        self.data.code = 'hukulcspha'
-                        self.data.website = poi_data['link'].strip() if poi_data['link'] is not None else None
-                        self.data.city = clean_city(poi_data['helyseg'])
-                        self.data.lat, self.data.lon = check_hu_boundary(poi_data['marker_position']['latitude'],
-                                                               poi_data['marker_position']['longitude'])
-                        self.data.street, self.data.housenumber, self.data.conscriptionnumber = extract_street_housenumber_better_2(
-                            poi_data['cim'])
-                        self.data.postcode = poi_data.get('irsz').strip()
-                        self.data.original = poi_data['cim']
-                        self.data.public_holiday_open = False
-                        self.data.add()
+                # soup = save_downloaded_soup('{}'.format(self.link), os.path.join(self.download_cache,
+                #                            self.filename), self.post, self.verify_link, headers=self.headers)
+                with open(os.path.join(self.download_cache, self.filename), 'r') as f:
+                    text = json.load(f)
+                    if text is not None:
+                        # text = json.loads(soup.get_text())
+                        for poi_data in text:
+                            try:
+                                if 'Kulcs patika' not in poi_data.get('nev'):
+                                    self.data.name = poi_data.get('nev').strip()
+                                    self.data.branch = None
+                                else:
+                                    self.data.name = 'Kulcs patika'
+                                    self.data.branch = poi_data.get('nev').strip()
+                                self.data.code = 'hukulcspha'
+                                if poi_data.get('link') is not None and poi_data.get('link') != '':
+                                    self.data.website = poi_data.get('link').strip() if poi_data.get('link')\
+                                        is not None else None
+                                if poi_data.get('helyseg') is not None and poi_data.get('helyseg') != '':
+                                    self.data.city = clean_city(poi_data.get('helyseg'))
+                                self.data.lat, self.data.lon = \
+                                    check_hu_boundary(poi_data.get('marker_position')['latitude'],
+                                                      poi_data.get('marker_position')['longitude'])
+                                if poi_data.get('cim') is not None and poi_data.get('cim') != '':
+                                    self.data.original = poi_data.get('cim')
+                                    self.data.street, self.data.housenumber, self.data.conscriptionnumber =\
+                                        extract_street_housenumber_better_2(poi_data.get('cim'))
+                                if poi_data.get('irsz') is not None and poi_data.get('irsz') != '':
+                                    self.data.postcode = poi_data.get('irsz').strip()
+                                self.data.public_holiday_open = False
+                                self.data.add()
+                            except Exception as e:
+                                logging.error(e)
+                                logging.error(poi_data)
+                                logging.error(traceback.print_exc())
         except Exception as e:
-            logging.error(traceback.print_exc())
             logging.error(e)
+            logging.error(traceback.print_exc())
