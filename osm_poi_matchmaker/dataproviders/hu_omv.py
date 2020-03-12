5af8c92e0fe8d014b1a25bbfe2ade0f5b0f2c32a
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-03-12 07:05:37 +0100

    The Hungarian OMV data provider is broken - this is the fix for it
    Resolving all of these problems:
    
    * The address has changed
    * Download headers, data structure has changed
    
    Fixes #58
    
    Uniform data providers
    
    Issue #57

--- osm_poi_matchmaker/dataproviders/hu_omv.py
+++ osm_poi_matchmaker/dataproviders/hu_omv.py
@@ -21,9 +21,8 @@ except ImportError as err:
 
 class hu_omv(DataProvider):
 
-
     def constains(self):
-        self.link = 'http://webgispu.wigeogis.com/kunden/omvpetrom/backend/getFsForCountry.php'
+        self.link = 'https://app.wigeogis.com/kunden/omvpetrom/data/getresults.php'
         self.POI_COMMON_TAGS = "'amenity': 'fuel', 'name': 'OMV', 'brand': 'OMV', 'fuel:diesel': 'yes', " \
                                "'fuel:octane_95': 'yes', 'air_conditioning': 'yes', 'brand:wikidata': 'Q168238', " \
                                "'brand:wikipedia': 'en:OMV', 'operator': 'OMV Hungária Kft.', " \
@@ -37,7 +36,8 @@ class hu_omv(DataProvider):
                                "'contact:youtube': 'https://www.youtube.com/user/omvofficial', " \
                                "'contact:phone': '+36 1 381 9700', 'contact:website': 'http://www.omv.hu/', "
         self.filename = self.filename + 'json'
-        self.post = {'BRAND': 'OMV', 'CTRISO': 'HUN', 'MODE': 'NEXTDOOR', 'ANZ': '5'}
+        self.post = {'BRAND': 'OMV', 'CTRISO': 'HUN', 'MODE': 'NEXTDOOR', 'ANZ': '5',
+                     'HASH': '23126a64295e2cf2a5e41f33fd4b0c416e09b0b8', 'TS': '1583951283'}
 
     def types(self):
         self.__types = [{'poi_code': 'huomvfu', 'poi_name': 'OMV', 'poi_type': 'fuel',
@@ -46,41 +46,48 @@ class hu_omv(DataProvider):
                  'osm_search_distance_safe': 200, 'osm_search_distance_unsafe': 60}]
         return self.__types
 
-
     def process(self):
         try:
             soup = save_downloaded_soup('{}'.format(self.link), os.path.join(self.download_cache, self.filename),
                                         self.post)
             if soup is not None:
                 text = json.loads(soup.get_text())
-                for poi_data in text['results']:
-                    self.data.name = 'OMV'
-                    self.data.code = 'huomvfu'
-                    self.data.postcode = poi_data.get('postcode').strip()
-                    self.data.city = clean_city(poi_data['town_l'])
-                    if poi_data['open_hours'] is not None:
-                        oho, ohc = clean_opening_hours(poi_data['open_hours'])
-                        if oho == '00:00' and ohc == '24:00':
-                            self.data.nonstop = True
-                            self.data.public_holiday_open = True
-                            oho, ohc = None, None
+                for poi_data in text:
+                    try:
+                        self.data.name = 'OMV'
+                        self.data.code = 'huomvfu'
+                        if poi_data.get('postcode') is not None and poi_data.get('postcode') != '':
+                            self.data.postcode = poi_data.get('postcode').strip()
+                        if poi_data.get('town_l') is not None and poi_data.get('town_l') != '':
+                            self.data.city = clean_city(poi_data.get('town_l'))
+                        if poi_data.get('open_hours') is not None:
+                            oho, ohc = clean_opening_hours(poi_data.get('open_hours'))
+                            if oho == '00:00' and ohc == '24:00':
+                                self.data.nonstop = True
+                                self.data.public_holiday_open = True
+                                oho, ohc = None, None
+                            else:
+                                self.data.public_holiday_open = False
                         else:
+                            oho, ohc = None, None
                             self.data.public_holiday_open = False
-                    else:
-                        oho, ohc = None, None
-                        self.data.public_holiday_open = False
-                    for i in range(0, 7):
-                        self.data.day_open(i, oho)
-                        self.data.day_close(i, ohc)
-                    self.data.original = poi_data['address_l']
-                    self.data.lat, self.data.lon = check_hu_boundary(poi_data['y'], poi_data['x'])
-                    self.data.street, self.data.housenumber, self.data.conscriptionnumber = extract_street_housenumber_better_2(
-                        poi_data['address_l'])
-                    if 'telnr' in poi_data and poi_data['telnr'] != '':
-                        self.data.phone = clean_phone_to_str(poi_data['telnr'])
-                    else:
-                        self.data.phone = None
-                    self.data.add()
+                        for i in range(0, 7):
+                            self.data.day_open(i, oho)
+                            self.data.day_close(i, ohc)
+                        self.data.lat, self.data.lon = check_hu_boundary(poi_data.get('y'), poi_data.get('x'))
+                        if poi_data.get('address_l') is not None and poi_data.get('address_l') != '':
+                            self.data.original = poi_data.get('address_l')
+                            self.data.street, self.data.housenumber, self.data.conscriptionnumber =\
+                                extract_street_housenumber_better_2(poi_data.get('address_l'))
+                        if poi_data.get('telnr') is not None and poi_data.get('telnr') != '':
+                            self.data.phone = clean_phone_to_str(poi_data.get('telnr'))
+                        else:
+                            self.data.phone = None
+                        self.data.add()
+                    except Exception as e:
+                        logging.error(e)
+                        logging.error(poi_data)
+                        logging.error(traceback.print_exc())
         except Exception as e:
-            logging.error(traceback.print_exc())
             logging.error(e)
+            logging.error(traceback.print_exc())
