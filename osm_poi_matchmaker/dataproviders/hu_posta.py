30dfdf978e97846c21b7cbfceda24a0c6853cfa7
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-03-13 09:03:15 +0100

    Refine search distances for Magyar Posta (HU) data provider
    Issue #25

--- osm_poi_matchmaker/dataproviders/hu_posta.py
+++ osm_poi_matchmaker/dataproviders/hu_posta.py
@@ -31,20 +31,28 @@ class hu_posta(DataProvider):
     def types(self):
         data = [{'poi_code': 'hupostapo', 'poi_name': 'Posta', 'poi_type': 'post_office',
                  'poi_tags': "{'amenity': 'post_office', " + POI_COMMON_TAGS + "}",
-                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300},
+                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta',
+                 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300,
+                 'osm_search_distance_unsafe': 300},
                 {'poi_code': 'hupostacse', 'poi_name': 'Posta csekkautomata', 'poi_type': 'vending_machine_cheques',
                  'poi_tags': "{'amenity': 'vending_machine', 'vending': 'cheques', " + POI_COMMON_TAGS + "}",
-                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300},
+                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta',
+                 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300,
+                 'osm_search_distance_unsafe': 300},
                 {'poi_code': 'hupostacso', 'poi_name': 'Posta csomagautomata',
                  'poi_type': 'vending_machine_parcel_pickup',
                  'poi_tags': "{'amenity': 'vending_machine', 'vending': 'parcel_pickup', " + POI_COMMON_TAGS + "}",
-                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': '(mpl|posta)', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200},
+                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': '(mpl|posta)',
+                 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200},
                 {'poi_code': 'hupostapp', 'poi_name': 'PostaPont', 'poi_type': 'post_office',
                  'poi_tags': "{'amenity': 'post_office', " + POI_COMMON_TAGS + "}",
-                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': '(postapont|posta)', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200},
+                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': '(postapont|posta)',
+                 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300,
+                 'osm_search_distance_unsafe': 300},
                 {'poi_code': 'hupostamp', 'poi_name': 'Mobilposta', 'poi_type': 'post_office',
                  'poi_tags': "{'amenity': 'post_office', " + POI_COMMON_TAGS + "}",
-                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200}]
+                 'poi_url_base': 'https://www.posta.hu', 'poi_search_name': 'posta',
+                 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 200}]
         return data
 
     def process(self):
