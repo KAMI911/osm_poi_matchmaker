a4007d3577aec6ec9429b5b23c4706dc31861a3c
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-03-17 21:48:26 +0100

    The query of shops is also deffective since epmpty fields are used in query
    Let's use real variables. Later maybe we should rename all not trivial filed names.
    
    Issue #69

--- osm_poi_matchmaker/libs/online_poi_matching.py
+++ osm_poi_matchmaker/libs/online_poi_matching.py
@@ -41,7 +41,7 @@ def online_poi_matching(args):
                 osm_query = db.query_osm_shop_poi_gpd(row.get('poi_lon'), row.get('poi_lat'),
                     comm_data.loc[comm_data['pc_id'] == row.get('poi_common_id')]['poi_type'].values[0],
                     row.get('poi_search_name'), row.get('poi_addr_street'),
-                    row.get('poi_addr_housenumber'), row.get('poi_addr_conscriptionnumber'), row.get('poi_addr_city'),
+                    row.get('poi_addr_housenumber'), row.get('poi_conscriptionnumber'), row.get('poi_city'),
                     row.get('osm_search_distance_perfect'), row.get('osm_search_distance_safe'),
                     row.get('osm_search_distance_unsafe'))
                 # Enrich our data with OSM database POI metadata
