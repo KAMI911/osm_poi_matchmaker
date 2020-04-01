992b3331bf296f93ad94f98b1629dd410ee231cb
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-04-01 07:38:52 +0200

    Uniforming Hungarian MOL data provider
    * Add additional fuel information
    
    Issue #57

--- osm_poi_matchmaker/dataproviders/hu_mol.py
+++ osm_poi_matchmaker/dataproviders/hu_mol.py
@@ -30,8 +30,16 @@ class hu_mol(DataProvider):
 
     def types(self):
         self.__types = [{'poi_code': 'humolfu', 'poi_name': 'MOL', 'poi_type': 'fuel',
-                 'poi_tags': "{'amenity': 'fuel', 'brand': 'MOL', 'operator': 'MOL Nyrt.', 'operator:addr': '1117 Budapest, Október huszonharmadika utca 18.', 'ref:vatin:hu': '10625790-4-44', 'contact:website': 'https://mol.hu/', 'contact:facebook': 'https://www.facebook.com/mol.magyarorszag/', 'contact:youtube': 'https://www.youtube.com/user/molgrouptv', 'contact:instagram': 'https://www.instagram.com/mol.magyarorszag/', 'brand:wikipedia': 'hu:MOL Magyar Olaj- és Gázipari Nyrt.', 'brand:wikidata': 'Q549181', 'ref:HU:company': '01-10-041683', " + POS_HU_GEN + PAY_CASH + "'fuel:diesel': 'yes', 'fuel:octane_95': 'yes', 'air_conditioning': 'yes'}",
-                 'poi_url_base': 'https://www.mol.hu', 'poi_search_name': 'mol', 'osm_search_distance_perfect': 2000, 'osm_search_distance_safe': 300, 'osm_search_distance_unsafe': 60}]
+            'poi_tags': "{'amenity': 'fuel', 'brand': 'MOL', 'operator': 'MOL Nyrt.', "
+            "'operator:addr': '1117 Budapest, Október huszonharmadika utca 18.', 'ref:vatin:hu': '10625790-4-44', "
+            "'contact:website': 'https://mol.hu/', 'contact:facebook': 'https://www.facebook.com/mol.magyarorszag/', "
+            "'contact:youtube': 'https://www.youtube.com/user/molgrouptv', "
+            "'contact:instagram': 'https://www.instagram.com/mol.magyarorszag/', "
+            "'brand:wikipedia': 'hu:MOL Magyar Olaj- és Gázipari Nyrt.', 'brand:wikidata': 'Q549181', "
+            "'ref:HU:company': '01-10-041683', " + POS_HU_GEN + PAY_CASH + "'fuel:diesel': 'yes', "
+            "'fuel:octane_95': 'yes', 'air_conditioning': 'yes'}",
+            'poi_url_base': 'https://www.mol.hu', 'poi_search_name': 'mol', 'osm_search_distance_perfect': 2000,
+            'osm_search_distance_safe': 300, 'osm_search_distance_unsafe': 60}]
         return self.__types
 
 
@@ -49,9 +57,17 @@ class hu_mol(DataProvider):
                     self.data.lat, self.data.lon = check_hu_boundary(poi_data['lat'], poi_data['lng'])
                     self.data.street, self.data.housenumber, self.data.conscriptionnumber = extract_street_housenumber_better_2(
                         poi_data['address'])
+                    self.data.public_holiday_open = True
                     self.data.truck = True if 'kamion_parkolo' in poi_data.get('servicesin') else False
                     self.data.food = True if 'fresh_corner' in poi_data.get('servicesin') else False
-                    self.data.rent_lpg_bottles = True if 'lpg' in poi_data.get('servicesin') else False
+                    self.data.rent_lpg_bottles = True if 'pb' in poi_data.get('servicesin') else False
+                    self.data.fuel_adblue = True if 'adblue' in poi_data.get('servicesin') else False
+                    self.data.fuel_lpg = True if 'lpg' in poi_data.get('servicesin') else False
+                    self.data.fuel_octane_95 = True
+                    self.data.fuel_diesel = True
+                    self.data.fuel_octane_100 = True
+                    self.data.fuel_diesel_gtl = True
+                    self.data.compressed_air = True
                     self.data.public_holiday_open = False
                     self.data.add()
         except Exception as e:
