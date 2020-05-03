cb4db7d6b559be2e4921e0aa99937274ceae674f
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-05-03 06:13:16 +0200

    Add comparation comments

--- osm_poi_matchmaker/libs/compare_strings.py
+++ osm_poi_matchmaker/libs/compare_strings.py
@@ -11,11 +11,15 @@ except ImportError as err:
     sys.exit(128)
 
 def compare_strings(string1, string2 = ''):
+    # New string
     if (string1 is '' or string1 is None) and (string2 is not '' and string2 is not None):
         return 'N'
+    # Deleted string
     elif (string1 is not '' and string1 is not None) and (string2 is '' or string2 is None):
         return 'D'
+    # Modified string
     elif  str(string1) != str(string2):
         return 'M'
+    # Equal string
     elif str(string1) == str(string2):
         return 'E'
