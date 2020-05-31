77c7a3428025b2f95394f302c685ad2b29feb251
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-05-31 12:25:53 +0200

    Add small handling script to restart matchmaking process

---
+++ restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker stop opm_osm_load_app &
+docker stop opm_poi_load_app &
+
+./build_opm_docker.sh
+./build_osmloader_docker.sh
+
+echo ""> ./import.log
+docker-compose up 2>&1 | tee -a ./import.log
+# docker stack deploy -c ./docker-compose.yml opm 2>&1 | tee -a ./import.log
+
