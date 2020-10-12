c303a8e19ec4100dc66b89329c301b2e001b6acf
Kálmán „KAMI” Szalai <kami911@gmail.com>
2020-10-12 21:02:51 +0200

    Add e-mobi support and change Mobiliti EVdata provider

--- osm_poi_matchmaker/dataproviders/hu_e-mobi_ev.py
+++ osm_poi_matchmaker/dataproviders/hu_e-mobi_ev.py
@@ -19,25 +19,26 @@ except ImportError as err:
     sys.exit(128)
 
 
-class hu_mobiliti_ev(DataProvider):
+class hu_e-mobi_ev(DataProvider):
 
     def constains(self):
-        self.link = os.path.join(config.get_directory_cache_url(), 'hu_mobiliti_ev.csv')
+        self.link = os.path.join(config.get_directory_cache_url(), 'hu_e-mobi_ev.csv')
         self.POI_COMMON_TAGS = ""
         self.filetype = FileType.csv
         self.filename = '{}.{}'.format(self.__class__.__name__, self.filetype.name)
 
     def types(self):
-        self.__types = [{'poi_code': 'humobilchs', 'poi_name': 'Mobiliti', 'poi_type': 'charging_station',
+        self.__types = [{'poi_code': 'huemobichs', 'poi_name': 'e-töltőpont', 'poi_type': 'charging_station',
                          'poi_tags': "{'amenity': 'charging_station', 'authentication:app': 'yes',"
                                      "'authentication:none': 'yes', 'authentication:membership_card': 'yes', "
-                                     "'operator': 'NKM Mobilitás Kft.', "
-                                     "'operator:addr': '1081 Budapest, II. János Pál pápa tér 20.', 'fee': 'yes',"
-                                     "'parking:fee': 'no', 'opening_hours': '24/7', 'ref:vatin': 'HU23443486', "
-                                     "'ref:vatin:hu': '23443486-2-42', 'ref:HU:company': '01-09-965868', "
-                                     "'contact:website': 'https://www.mobiliti.hu/emobilitas',"
-                                     "'contact:email': 'help@mobiliti.hu', 'contact:phone': '+36 62 565 758'}",
-                         'poi_url_base': 'https://www.mobiliti.hu', 'poi_search_name': '(mobility)',
+                                     "'operator': 'e-Mobi Elektromobilitás Nonprofit Kft.', "
+                                     "'operator:addr': '1037 Budapest, Montevideo utca 2/C', 'fee': 'yes',"
+                                     "'parking:fee': 'no', 'opening_hours': '24/7', 'ref:vatin': 'HU25539431', "
+                                     "'ref:vatin:hu': ' 25539431-2-41', 'ref:HU:company': '01 09 281052', "
+                                     "'alt_name': 'e-mobi', 'contact:website': 'https://e-mobi.hu/hu',"
+                                     "'contact:email': 'kapcsolat@e-mobi.hu', 'contact:phone': '+36 80 210 012', "
+                                     "'contact:facebook': 'https://www.facebook.com/elektromobilitas' }",
+                         'poi_url_base': 'https://www.mobiliti.hu', 'poi_search_name': '(e-mobi|emobi|e-töltőpont)',
                          'osm_search_distance_perfect': 50, 'osm_search_distance_safe': 30,
                          'osm_search_distance_unsafe': 10},
                         ]
@@ -49,8 +50,8 @@ class hu_mobiliti_ev(DataProvider):
             if csv is not None:
                 poi_dict = csv.to_dict('records')
                 for poi_data in poi_dict:
-                    self.data.name = 'Mobiliti'
-                    self.data.code = 'humobilchs'
+                    self.data.name = 'e-töltőpont'
+                    self.data.code = 'huemobichs'
                     self.data.ref = poi_data.get('Mobiliti azonosító')
                     self.data.branch = poi_data.get('Töltőpont neve')
                     self.data.postcode = poi_data.get('Irányító szám')
