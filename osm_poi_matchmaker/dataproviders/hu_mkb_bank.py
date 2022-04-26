01f433f73b4d2d8c73268fcdb84fe284e70985d0
Kálmán „KAMI” Szalai <kami911@gmail.com>
2022-04-26 11:18:27 +0200

    Add 'mkb' for search for better matching

--- osm_poi_matchmaker/dataproviders/hu_mkb_bank.py
+++ osm_poi_matchmaker/dataproviders/hu_mkb_bank.py
@@ -44,7 +44,7 @@ class hu_mkb_bank(DataProvider):
         self.__types = [
             {'poi_code': 'humkbbank', 'poi_name': 'MKB Bank', 'poi_type': 'bank',
              'poi_tags': humkbbank, 'poi_url_base': 'https://www.mkb.hu',
-             'poi_search_name': '(mkb bank)',
+             'poi_search_name': '(mkb|mkb bank)',
              'poi_search_avoid_name': '(otpbank|otp|otp bank|raiffeisenbank|raiffeisen bank|kh bank|k&h|raiffeisen|budapest bank|takarék bank|takarék)',
              'osm_search_distance_perfect': 300, 'osm_search_distance_safe': 100,
              'osm_search_distance_unsafe': 40},
