4c3fdec7f61ae16c804a02a49867e1d830356227
Kálmán „KAMI” Szalai <kami911@gmail.com>
2022-04-30 13:37:45 +0200

    Fix contact tag small caps

--- osm_poi_matchmaker/libs/file_output.py
+++ osm_poi_matchmaker/libs/file_output.py
@@ -391,7 +391,7 @@ def generate_osm_xml(df, session=None):
                             tags['contact:' + tr] = tags.pop(tr, None)
                         # rewrite email and website as small caps
                         if tr in ['email', 'website' ]:
-                            if isinstance(str, tags.get('contact:' + tr)) and tags.get('contact:' + tr) is not None:
+                            if isinstance(str, tags.get('contact:' + tr)) is True and isinstance(str, tags.get('contact:' + tr)) is True:
                                 tags['contact:' + tr] = str(tags.get('contact:' + tr)).lower()
             except Exception as e:
                 logging.exception('Exception occurred: {}'.format(e))
