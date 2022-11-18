4ae358e9df9395ba871adff165df5a5190d3be17
kalman Szalai <kami911@gmail.com>
2022-11-18 16:39:40 +0100

    The columns name are okey here so we dont want to rewrite them

--- osm_poi_matchmaker/dao/data_handlers.py
+++ osm_poi_matchmaker/dao/data_handlers.py
@@ -233,8 +233,6 @@ def search_for_postcode(session, city_name):
 def insert_poi_dataframe(session, poi_df, raw = True):
     if raw is True:
         poi_df.columns = POI_COLS_RAW
-    else:
-        poi_df.columns = POI_COLS
     poi_df[['poi_postcode']] = poi_df[['poi_postcode']].fillna('0000')
     poi_df[['poi_postcode']] = poi_df[['poi_postcode']].astype('int32')
     poi_dict = poi_df.to_dict('records')
