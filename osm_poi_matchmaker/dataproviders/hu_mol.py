715d6075a2816bf9efa450dd250eca14246fb73f
kalman Szalai <kami911@gmail.com>
2022-11-21 21:45:56 +0100

    Fix an error with Hungarian MOL dataprovider

--- osm_poi_matchmaker/dataproviders/hu_mol.py
+++ osm_poi_matchmaker/dataproviders/hu_mol.py
@@ -87,7 +87,7 @@ class hu_mol(DataProvider):
                         if ' Sziget ' in poi_data.get('name'):
                             self.data.code = 'humolwfu'
                         else:
-                            if 'fresh_corner' in poi_data.get('servicesin') and \ 
+                            if 'fresh_corner' in poi_data.get('servicesin') and \
                               not ('shop' in poi_data.get('servicesin') or \
                               ('adblue' in poi_data.get('servicesin') or ('matrica_magyar' in poi_data.get('servicesin')))):
                                 self.data.code = 'humolfaf'
