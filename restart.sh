cfb79c8a8852b8f6f09b0e0e4ea0cab07539ecb9
Kálmán „KAMI” Szalai <kami911@gmail.com>
2025-01-13 08:08:46 +0100

    Change docker compose usage

--- restart.sh
+++ restart.sh
@@ -13,5 +13,5 @@ docker stop opm_poi_load_app
 ./build_osmloader_docker.sh
 
 echo "" >./import.log
-docker-compose up 2>&1 | tee -a ./import.log
+docker compose up 2>&1 | tee -a ./import.log
 # docker stack deploy -c ./docker-compose.yml opm 2>&1 | tee -a ./import.log
