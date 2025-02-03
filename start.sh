a0ce5be27359dec8fde80521b3bc30239c3408a5
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2025-02-03 11:11:08 +0100

    Change 'docker-compose' command to 'docker compose'
    This is to migrate to Docker Compose V2.
    Reference:
    https://docs.docker.com/compose/releases/migrate/#docker-compose-vs-docker-compose

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ mkdir -p ./container/{dbdata,importdata,osmdata,downloaddata}
 
 docker pull postgis/postgis:13-3.0
 
-docker-compose up 2>&1 | tee -a ./import.log
+docker compose up 2>&1 | tee -a ./import.log
