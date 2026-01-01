4cc74045909803be273729d515bec65ad29d1362
Kálmán „KAMI” Szalai <kami911@gmail.com>
2026-01-01 13:10:46 +0100

    Fix operator name

--- osm_poi_matchmaker/dataproviders/hu_posta.py
+++ osm_poi_matchmaker/dataproviders/hu_posta.py
@@ -25,9 +25,7 @@ class hu_posta(DataProvider):
 
     def contains(self):
         self.link = 'https://httpmegosztas.posta.hu/PartnerExtra/OUT/PostInfo.xml'
-        self.tags = {'operator': 'Magyar Posta Zrt.',
-                     'operator:addr': '1138 Budapest, Dunavirág utca 2-6.', 'ref:HU:vatin': '10901232-2-44',
-                     'ref:vatin': 'HU10901232', 'brand:wikipedia': 'hu:Magyar Posta Zrt.', 'brand:wikidata': 'Q145614',
+        self.tags = {'ref:vatin': 'HU10901232', 'brand:wikipedia': 'hu:Magyar Posta Zrt.', 'brand:wikidata': 'Q145614',
                      'contact:email': 'ugyfelszolgalat@posta.hu', 'contact:phone': '+36 1 767 8200',
                      'contact:facebook': 'https://www.facebook.com/MagyarPosta',
                      'contact:youtube': 'https://www.youtube.com/user/magyarpostaofficial',
@@ -38,12 +36,16 @@ class hu_posta(DataProvider):
             self.__class__.__name__, self.filetype.name)
 
     def types(self):
-        hupostapo = {'amenity': 'post_office', 'brand': 'Magyar Posta'}
+        hupostapo = {'amenity': 'post_office', 'brand': 'Magyar Posta', 'operator': 'Magyar Posta Zrt.',
+                     'operator:addr': '1138 Budapest, Dunavirág utca 2-6.', 'ref:HU:vatin': '10901232-2-44',}
         hupostapo.update(self.tags)
-        hupostacse = {'amenity': 'vending_machine', 'vending': 'cheques', 'brand': 'Magyar Posta'}
+        hupostacse = {'amenity': 'vending_machine', 'vending': 'cheques', 'brand': 'Magyar Posta',
+                      'operator': 'Magyar Posta Zrt.', 'operator:addr': '1138 Budapest, Dunavirág utca 2-6.',
+                      'ref:HU:vatin': '10901232-2-44',}
         hupostacse.update(self.tags)
         hupostacso = {'amenity': 'parcel_locker', 'parcel_mail_in': 'yes', 'brand': 'MPL', 'parcel_pickup': 'yes',
-                      'colour': 'green', 'material': 'metal', 'refrigerated': 'no'}
+                      'colour': 'green', 'material': 'metal', 'refrigerated': 'no', 'operator': 'Magyar Posta Zrt.',
+                      'operator:addr': '1138 Budapest, Dunavirág utca 2-6.', 'ref:HU:vatin': '10901232-2-44',}
         hupostacso.update(self.tags)
         hupostapp = {'post_office': 'post_partner', 'post_office:brand': 'PostaPont',
                      'post_office:brand:wikidata': 'Q145614'}
