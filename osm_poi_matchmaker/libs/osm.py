9b8ff26ccc06d675e27e87f3346532f4abbd12f2
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2026-08-01 17:56:32 +0200

    Convert coordinates to float explicitly to avoid exceptions

--- osm_poi_matchmaker/libs/osm.py
+++ osm_poi_matchmaker/libs/osm.py
@@ -37,7 +37,7 @@ def query_osm_postcode_gpd(session, lon, lat):
         FROM planet_osm_polygon, (SELECT ST_SetSRID(ST_MakePoint(:lon, :lat),4326) as geom) point
         WHERE boundary='postal_code' and ST_Contains(way, point.geom) ORDER BY name LIMIT 1;''')
     try:
-        data = session.execute(query, {'lon': lon, 'lat': lat}).first()
+        data = session.execute(query, {'lon': float(lon), 'lat': float(lat)}).first()
     except Exception as e:
         logging.error(e)
         logging.exception('Exception occurred')
@@ -97,7 +97,7 @@ def query_osm_city_name_gpd(session, lon, lat):
         FROM planet_osm_polygon, (SELECT ST_SetSRID(ST_MakePoint(:lat,:lon),4326) as geom) point
         WHERE admin_level='8' and ST_Contains(way, point.geom) ORDER BY name LIMIT 1;''')
     try:
-        data = session.execute(query, {'lon': lon, 'lat': lat}).first()
+        data = session.execute(query, {'lon': float(lon), 'lat': float(lat)}).first()
     except Exception as e:
         logging.error(e)
         logging.exception('Exception occurred')
